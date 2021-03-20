548cfb5c12210bcaab547edf89f5d2b9c1ac45d1
nemisisfrank <75736666+nemisisfrank@users.noreply.github.com>
2021-03-20 15:16:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var gameState=0,database,playerCount;
+var form,player,game;
+function setup(){
+    database=firebase.database()
+    createCanvas(500,500);
+game=new Game()
+game.getState();
+game.start();
+}
+function draw(){
+
+}
